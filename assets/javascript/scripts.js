61f457472e919a314a9124c3f165212beae3c317
David Kirby <davidkirbymusician@gmail.com>
2023-05-08 22:12:00 +0000

    onclick for timer overwritten by event listener, fix attempted.

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,30 +1,25 @@
 // players
 
-let player1 = {
-    name: "";
-    score: 0;
-    choice: "";
+const player1 = {
+    name: "",
+    score: 0,
+    choice: "",
 }
 
-let player2 = {
-    name: "";
-    score: 0;
-    choice: "";
+const player2 = {
+    name: "",
+    score: 0,
+    choice: "",
 }
 
 // playerchoices
-let playerChoices = ["rock","paper","scissors","lizard","spock"]; 
-
-
-// timer
-// function timerBegin(){
-
-// }
+const playerChoices = ["rock","paper","scissors","lizard","spock"]; 
 
 
 document.addEventListener("DOMContentLoaded", function() {
     let button = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
     button.addEventListener("click", function() {
     let countdown = setInterval(function() {
         let timerElement = document.getElementById('timer');
@@ -32,41 +27,42 @@ document.addEventListener("DOMContentLoaded", function() {
         currentTime--;
         timerElement.innerHTML = currentTime;
         if (currentTime < 1) {
-            clearInterval(countdown)
-        }
-    }, 1000);
-});
-
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            // if (this.getAttribute("value") === "rock1" || "paper1" || "scissors1" || "lizard1" || "spock1")
-            // {
-                let player1Choice = this.getAttribute("value");
-                switch(player1Choice) {
-                    case "rock1":
-                        p1Choice = "rock";
-                        break;
-                    case "scissors1":
-                        p1Choice = "scissors";
-                        break;
-                    case "paper1":
-                        p1Choice = "paper" ;
-                        break
-                    case "lizard1":
-                        p1Choice = "lizard";
-                        break;
-                    case "spock1":
-                        p1Choice = "spock";
-                        break;
-                };
-            document.getElementById("p1Choice").innerHTML = "Player 1 has chosen: " + p1Choice;
-            player1.choice = p1Choice;
-            console.log(p1choice);
-            // };
+            clearInterval(countdown);
+                }
+            }, 1000);
         });
-    };
-})
+    }   
 
+    // for (let button of buttons) {
+    //     button.addEventListener("click", function() {
+    //         // if (this.getAttribute("value") === "rock1" || "paper1" || "scissors1" || "lizard1" || "spock1")
+    //         // {
+    //             let player1Choice = this.getAttribute("value");
+    //             switch(player1Choice) {
+    //                 case "rock1":
+    //                     p1Choice = "rock";
+    //                     break;
+    //                 case "scissors1":
+    //                     p1Choice = "scissors";
+    //                     break;
+    //                 case "paper1":
+    //                     p1Choice = "paper" ;
+    //                     break
+    //                 case "lizard1":
+    //                     p1Choice = "lizard";
+    //                     break;
+    //                 case "spock1":
+    //                     p1Choice = "spock";
+    //                     break;
+    //             };
+    //         document.getElementById("p1Choice").innerHTML = "Player 1 has chosen: " + p1Choice;
+    //         player1.choice = p1Choice;
+    //         console.log(p1choice);
+    //         // };
+    //     });
+    // };
+
+})
 
 
 
@@ -91,4 +87,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // fontRefactor();
 // window.addEventListener("resize", fontRefactor);
-
+// })

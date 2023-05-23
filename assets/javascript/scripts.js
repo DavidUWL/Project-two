ff015b29558763e8ce293d430166d2e70632d6cf
David Kirby <davidkirbymusician@gmail.com>
2023-05-23 22:56:22 +0000

    refactored player choice print to be less verbose. Fixed bug with timer where event listener was removed.

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -49,80 +49,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
     beginTimer();
 
-    player1ChoicePrint();
-
-    player2ChoicePrint();
+    logPlayerChoice();
 });
 
-function player2ChoicePrint() {
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("value") === "rock2" || "paper2" || "scissors2" || "lizard2" || "spock2") {
-                let player2Choice = this.getAttribute("value");
-                switch (player2Choice) {
-                    case "rock2":
-                        p2Choice = "rock";
-                        break;
-                    case "scissors2":
-                        p2Choice = "scissors";
-                        break;
-                    case "paper2":
-                        p2Choice = "paper";
-                        break;
-                    case "lizard2":
-                        p2Choice = "lizard";
-                        break;
-                    case "spock2":
-                        p2Choice = "spock";
-                        break;
-                        default:
-                        p2Choice = "";
-                        break;
-                            
-                };
-                updatePlayerUIChoices();
-                player2.choice = p2Choice;
-            };
-        });
-    };
-};
 
-function player1ChoicePrint() {
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("value") === "rock1" || "paper1" || "scissors1" || "lizard1" || "spock1") {
-                let player1Choice = this.getAttribute("value");
-                switch (player1Choice) {
-                    case "rock1":
-                        p1Choice = "rock";
-                        break;
-                    case "scissors1":
-                        p1Choice = "scissors";
-                        break;
-                    case "paper1":
-                        p1Choice = "paper";
-                        break;
-                    case "lizard1":
-                        p1Choice = "lizard";
-                        break;
-                    case "spock1":
-                        p1Choice = "spock";
-                        break;
-                        default:
-                        p1Choice = "";
-                        break;
-                };
-                updatePlayerUIChoices();
-                player1.choice = p1Choice;
-            };
-        });
-    };
+
+function logPlayerChoice() {
+  for (let button of buttons) {
+    button.addEventListener("click", function() {
+      if (this.classList.contains("p1button")) {
+        let p1Choice = this.getAttribute("value");
+        player1.choice = p1Choice;
+        // console.log(player1.choice);
+        updatePlayerUIChoices();
+      } else {
+        let p2Choice = this.getAttribute("value");
+        player2.choice = p2Choice;
+        // console.log(player2.choice);
+        updatePlayerUIChoices();
+      };
+    });
+
+  };
 };
 
 function beginTimer() {
     for (let button of buttons) {
         button.addEventListener("click", function beginTimerCount() {
-            button.removeEventListener("click", beginTimerCount);
+            // button.removeEventListener("click", beginTimerCount);
             if (timerAllowed) {
                 timerAllowed = false;
                 let currentTime = 5;
@@ -190,8 +144,17 @@ function getScoreCard() {
 };
 
 function updatePlayerUIChoices() {
-    document.getElementById("p1Choice").innerHTML = "Player 1 has chosen: " + player1.choice;
-    document.getElementById("p2Choice").innerHTML = "Player 2 has chosen: " + player2.choice;
+    if (player1.choice) {
+        document.getElementById("p1Choice").innerHTML = "Player 1 has chosen: " + player1.choice;
+    } else {
+        document.getElementById("p1Choice").innerHTML = "This Player has not chosen."
+    };
+
+    if (player2.choice) {
+        document.getElementById("p2Choice").innerHTML = "Player 2 has chosen: " + player2.choice;
+    } else {
+        document.getElementById("p2Choice").innerHTML = "This Player has not chosen." 
+    }
 };
 
 function updateScoreUI() {
